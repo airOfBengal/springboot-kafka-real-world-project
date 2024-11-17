f14ac82dd43045c0112d49b566fb5d04938a28da
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2024-11-17 11:29:15 +0600

    add mysql database for wikimedia data

--- kafka-consumer-database/src/main/java/com/example/springboot/services/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/com/example/springboot/services/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package com.example.springboot.services;
 
+import com.example.springboot.entity.WikimediaData;
+import com.example.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,8 +11,18 @@ import org.springframework.stereotype.Service;
 public class KafkaDatabaseConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
+    private WikimediaDataRepository wikimediaDataRepository;
+
+    public KafkaDatabaseConsumer(WikimediaDataRepository wikimediaDataRepository) {
+        this.wikimediaDataRepository = wikimediaDataRepository;
+    }
+
     @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(String message) {
         LOGGER.info(String.format("Message received: %s", message));
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(message);
+
+        wikimediaDataRepository.save(wikimediaData);
     }
 }
